52b0179eaf6f4b64de3214f77a432110b4f49e40
Ravi Ranjan <124711830+Ranjan-Ravii@users.noreply.github.com>
2024-07-27 18:41:38 +0530

    Add files via upload
    file uploaded

---
+++ script.js
@@ -0,0 +1,82 @@
+const giveUpButton  = document.querySelector(".giveUp");
+const playAgainButton  = document.querySelector('.palyAgain');
+const numBox = document.querySelector('.num_box')
+const result = document.querySelector('.result')
+const guessButton  = document.querySelector('.guessbtn')
+const countdownDisplay  = document.querySelector('.countdown')
+
+
+let countdownValue = 10
+const randomNum = Math.floor((Math.random() * 100) + 1)
+console.log(randomNum)
+var x = document.querySelector(".myAudio");
+
+function playAudio() {
+  x.play();
+}
+
+
+giveUpButton.addEventListener('click', function () {
+    numBox.value = '';
+    result.innerHTML = "You Loosed the Game."
+    countdownValue = 10;
+    countdownDisplay.innerHTML = countdownValue;
+    
+    
+})
+
+playAgainButton.addEventListener('click', function () {
+    numBox.value = '';
+    result.innerHTML = "Let's Try and Guess it."
+    countdownValue = 10;
+    countdownDisplay.innerHTML = countdownValue;
+    resetGame();
+    
+    
+})
+
+
+
+// for countdown 
+// guessbtn.addEventListener('click', function () {
+//     if (countdownValue > 0) {
+//         countdownValue -= 1;
+//         countdown.innerHTML = countdownValue
+//     }
+//     else {
+//         result.innerHTML = "Loosed..! No chance left."
+//     }
+// })
+
+// taking input and matching with the result
+
+
+
+guessButton.addEventListener('click', function () {
+    if (countdownValue > 0) {
+        countdownValue -= 1;
+        countdownDisplay.innerHTML = countdownValue;
+
+        if (randomNum == numBox.value) {
+            playAudio()
+            result.innerHTML = "Congratulations! You won.";
+        } else if (randomNum > numBox.value) {
+            numBox.value = '';
+            result.innerHTML = "Expected num is Greater";
+        }
+        else if(randomNum < numBox.value){
+            numBox.value = '';
+            result.innerHTML = "Expected num is Smaller";
+        }
+        if (countdownValue === 0) {
+            numBox.value = '';
+            result.innerHTML = `You lost..! it's ${randomNum}`;
+        }
+    }
+
+});
+
+function resetGame(){
+    location.reload()
+}
+
